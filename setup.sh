73b0a9f27dad81586bd131d31d3ac386b918fda7
田中　悠樹 <yuki_tanaka@r.recruit.co.jp>
2017-05-19 11:17:59 +0900

    initial commit for setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-dot_profiles=( $( ls -d . ) )
+dot_profiles=( $( ls -ed .[!.]* ) )
 
 echo "Found profiles: ${dot_profiles[@]}"
+ignore_files=(.git)
+pwd=$(pwd)
 
+echo "Creating symbolic link to under home directory"
+for profile in ${dot_profiles[@]/$ignore_files}
+do
+  ln -s ${pwd}/i${profile} $HOME/${profile}
+  echo "Successfully created symoblic link for profile ${profile}"
+done
 
